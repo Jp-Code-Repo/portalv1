ffd4087a9c923d66cff4f8da8821a0971b1d58fd
Jp-Code-Repo <jpbuzz6100@gmail.com>
2026-07-17 17:20:48 +0800

    Identity Foundation

--- services/portal-service/routes/web.php
+++ services/portal-service/routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group([
+    'prefix' => 'portal',
+    'namespace' => 'App\Http\Controllers',
+], function () {
+
+    Route::get('/', 'UserController@index');
+
+});
